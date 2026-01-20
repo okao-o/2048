fc5e5c4ebfbd54863ffda2239cf396e41c72ef0e
ｵｶｵ <aotakokeshi@gmail.com>
2026-01-21 03:51:01 +0900

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,5 +1,5 @@
 const sheetURL =
-  "https://docs.google.com/forms/d/e/1FAIpQLSfyP3Uit3d8wD-qpFTifTIfP2S_LWQX6WuwlWeADqVbhSMDdQ//viewform";
+  "https://docs.google.com/forms/d/e/1FAIpQLSfyP3Uit3d8wD-qpFTifTIfP2S_LWQX6WuwlWeADqVbhSMDdQ/gviz/tq?tqx=out:json";
 
 fetch(sheetURL)
   .then(res => res.text())
