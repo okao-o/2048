e3e631dcc552644a75f8aae7eb90a8cad94561a4
ｵｶｵ <aotakokeshi@gmail.com>
2026-01-21 02:37:39 +0900

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,24 +1,3 @@
-function submitScore(nickname, score) {
-  const formURL = "https://docs.google.com/forms/d/e/XXXXXXXXXXXX/formResponse";
-
-  const data = new FormData();
-  data.append("entry.apiproxy78ffdedc057b5c9893cb73e5e93c24fd369fc6a30.1117175448", nickname);
-  data.append("entry.apiproxy1bebbc6a250f2dcc00b475b1d15c79aa0ebf8b180.904413126", score);
-
-  fetch(formURL, {
-    method: "POST",
-    mode: "no-cors",
-    body: data
-  });
-}
-
-const nickname = localStorage.getItem("nickname");
-if (nickname) {
-  submitScore(nickname, score);
-}
-
-
-
 const sheetURL =
   "https://docs.google.com/spreadsheets/d/1GuWisZRu6ameV_UfD69I6RDWZeAnI2MmduurxAD9VGQ/gviz/tq?tqx=out:json";
 
@@ -44,3 +23,4 @@ fetch(sheetURL)
       list.appendChild(li);
     });
   });
+
