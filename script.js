31e3bb5c949aa2071627f145717bb55dca33b5eb
ｵｶｵ <aotakokeshi@gmail.com>
2026-01-22 13:30:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const value = board[i][j];
 
         cell.className = "cell";
-        cell.textContent = "";
+
         
 
 　if (value !== 0) {
